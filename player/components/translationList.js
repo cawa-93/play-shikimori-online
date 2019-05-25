88b7267afdf383946169c90af193c8ab83a2b478
Alex Kozack <kozackunisoft@gmail.com>
2019-05-25 22:29:59 +0300

    Фильтрация переводов

--- player/components/translationList.js
+++ player/components/translationList.js
@@ -13,16 +13,49 @@ export default {
 
   template: `<section class="translation-list" v-if="translations && translations.length">
 
+    <ul>
+      <li>
+        <label for="typeKind-voiceRu">Озвучка</label>
+        <input type="radio" name="typeKind" v-model="filters.type" value="voiceRu" id="typeKind-voiceRu">
+      </li>
+
+      <li>
+        <label for="typeKind-subRu">Русские Субтиитры</label>
+        <input type="radio" name="typeKind" v-model="filters.type" value="subRu" id="typeKind-subRu">
+      </li>
+
+      <li>
+        <label for="typeKind-subEn">Английские Субтиитры</label>
+        <input type="radio" name="typeKind" v-model="filters.type" value="subEn" id="typeKind-subEn">
+      </li>
+      <li>
+        <label for="typeKind-subJa">Японские Субтиитры</label>
+        <input type="radio" name="typeKind" v-model="filters.type" value="subJa" id="typeKind-subJa">
+      </li>
+      <li>
+        <label for="typeKind-raw">Оригинал</label>
+        <input type="radio" name="typeKind" v-model="filters.type" value="raw" id="typeKind-raw">
+      </li>
+    </ul>
+
 
     <ul>
-      <li v-for="translation in translations" :key="translation.id">
-      {{translation}}
-        <a :href="translation.url">{{translation.authorsSummary}}</a>
+      <li v-for="translation in filteredTranslations" :key="translation.id">
+      {{translation.type}}
+        <a :href="translation.url">{{translation.authorsSummary || 'Неизвестный'}}</a>
       </li>
     </ul>
 
   
   </section>`,
+
+  data() {
+    return {
+      filters: {
+        type: 'voiceRu',
+      }
+    }
+  },
 
   computed: {
     translations() {
@@ -30,6 +63,11 @@ export default {
         return []
       }
       return this.$store.getters['player/currentEpisode'].translations
+    },
+
+    filteredTranslations() {
+      return this.translations
+        .filter(translation => translation.type === this.filters.type)
     }
   },
 
