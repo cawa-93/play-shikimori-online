298004427c52c4540716010099cf7fdd6219d242
Alex Kozack <kozackunisoft@gmail.com>
2019-05-30 14:41:20 +0300

    Исправлена ошибка определения продолжительности видео

--- src/content-scripts/anime365-player-events.js
+++ src/content-scripts/anime365-player-events.js
@@ -3,49 +3,73 @@ if (window.parent) {
   const video = document.querySelector('video#main-video')
 
   if (video) {
-    const events = ["abort", "canplay", "canplaythrough", "durationchange", "emptied", "ended", "error", "loadeddata", "loadedmetadata", "loadstart", "pause", "play", "playing", "progress", "ratechange", "seeked ", "seeking", "stalled", "suspend", "timeupdate", "volumechange", "waiting"]
+    const events = ["pause", "play"]
     events.forEach(eventName => {
       video.addEventListener(eventName, (event) => {
-        const currentTime = video.currentTime
-        const duration = video.duration
-        const paused = video.paused
-
         window.parent.postMessage({
           name: eventName,
-          currentTime,
-          duration,
-          paused,
         }, '*')
-
-
-        if (eventName === 'timeupdate') {
-          /** @type {HTMLButtonElement} */
-          let nextEpisodeButton = document.querySelector('.next-episode')
-
-          if (duration - currentTime < 120) {
-            if (!nextEpisodeButton) {
-              nextEpisodeButton = document.createElement('button')
-              nextEpisodeButton.innerText = "Следующий епизод"
-              nextEpisodeButton.classList.add('next-episode')
-              nextEpisodeButton.hidden = true
-              document.querySelector('#main-video').appendChild(nextEpisodeButton)
-
-              nextEpisodeButton.onclick = function () {
-                window.parent.postMessage({
-                  name: 'mark-as-watched',
-                  currentTime: video.currentTime,
-                  duration: video.duration,
-                  paused: video.paused,
-                }, '*')
-              }
-            }
-
-            nextEpisodeButton.hidden = false
-          } else if (nextEpisodeButton) {
-            nextEpisodeButton.hidden = true
-          }
-        }
       });
     })
+
+
+    function videoChunksObserverHandler() {
+      const sources = JSON.parse(video.dataset.sources)
+      const selectedSource = sources.find(s => s.urls.includes(video.src))
+      if (!selectedSource || !video.src) {
+        return
+      }
+
+      if (selectedSource.urls[selectedSource.urls.length - 1] === video.src) {
+        video.addEventListener('timeupdate', timeupdateHandler)
+        video.addEventListener('ended', endedHandler)
+      } else {
+        video.removeEventListener('timeupdate', timeupdateHandler)
+        video.removeEventListener('ended', endedHandler)
+      }
+    }
+
+    const videoChunksObserver = new MutationObserver(videoChunksObserverHandler)
+    const observerConfig = { attributeFilter: ['src'], subtree: false, childList: false }
+    videoChunksObserver.observe(video, observerConfig)
+
+    function timeupdateHandler() {
+      const currentTime = video.currentTime
+      const duration = video.duration
+      /** @type {HTMLButtonElement} */
+      let nextEpisodeButton = document.querySelector('.next-episode')
+
+      if (duration - currentTime < 120) {
+        if (!nextEpisodeButton) {
+          nextEpisodeButton = document.createElement('button')
+          nextEpisodeButton.innerText = "Следующий епизод"
+          nextEpisodeButton.classList.add('next-episode')
+          nextEpisodeButton.hidden = true
+          document.querySelector('#main-video').appendChild(nextEpisodeButton)
+
+          nextEpisodeButton.onclick = function () {
+            window.parent.postMessage({
+              name: 'mark-as-watched',
+              currentTime: video.currentTime,
+              duration: video.duration,
+              paused: video.paused,
+            }, '*')
+          }
+        }
+
+        nextEpisodeButton.hidden = false
+      } else if (nextEpisodeButton) {
+        nextEpisodeButton.hidden = true
+      }
+    }
+
+    function endedHandler() {
+      window.parent.postMessage({
+        name: 'ended',
+      }, '*')
+    }
+
+
   }
+
 }
