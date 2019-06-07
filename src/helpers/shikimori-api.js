8b97dd57560f18ab3bf7071e124fce5d83261265
Alex Kozack <kozackunisoft@gmail.com>
2019-06-07 17:12:35 +0300

    Определение наиболее подходящего домена Шикимори

--- src/helpers/shikimori-api.js
+++ src/helpers/shikimori-api.js
@@ -1,7 +1,30 @@
-function shikimoriAPI(path, options = {}) {
+async function shikimoriAPI(path, options = {}) {
+  let domain = sessionStorage.getItem('shiki-domain')
+  if (!domain) {
+    domain = await getShikiDomain()
+    sessionStorage.setItem('shiki-domain', domain)
+  }
+  const url = `https://${domain}/api${path}`
+  return callAPI(url, options)
+}
 
+async function getShikiDomain() {
+  let domain = 'shikimori.one'
+  try {
+    const resp = await callAPI('https://shikimori.org/api/users/whoami')
+    if (resp && resp.id) {
+      domain = 'shikimori.org'
+    }
+  } catch (e) { }
+
+  return domain
+
+
+}
+
+function callAPI(fullURL, options = {}) {
   return new Promise((resolve, reject) => {
-    const url = 'https://shikimori.org/api' + path
+
     let headers = {
       "Accept": "application/json",
       "Content-Type": "application/json",
@@ -13,7 +36,7 @@ function shikimoriAPI(path, options = {}) {
 
     chrome.runtime.sendMessage({
       contentScriptQuery: 'fetchUrl',
-      url,
+      url: fullURL,
       options,
     },
       ({ response, error }) => {
