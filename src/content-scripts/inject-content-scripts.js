ab1700417e46c0a98aeb8b4fb902fa6939e9bcf8
Alex Kozack <kozackunisoft@gmail.com>
2019-06-22 16:40:47 +0300

    Исправлено поведение кнопки в плеере на Firefox

--- src/content-scripts/inject-content-scripts.js
+++ src/content-scripts/inject-content-scripts.js
@@ -1,11 +1,11 @@
-function injectScript(file, node) {
-  var th = document.getElementsByTagName(node)[0];
-  var s = document.createElement('script');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', file);
-  th.appendChild(s);
-}
+
 const extensionId = (new URL(location.href)).searchParams.get('extension-id')
 if (extensionId === chrome.runtime.id) {
-  injectScript(chrome.runtime.getURL('content-scripts/anime365-player-events.js'), 'body');
+  window.addEventListener('load', () => {
+    const s = document.createElement('script');
+    s.setAttribute('type', 'text/javascript');
+    s.setAttribute('src', chrome.runtime.getURL('content-scripts/anime365-player-events.js'));
+    document.body.appendChild(s);
+  });
 }
+
