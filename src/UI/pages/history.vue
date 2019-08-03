09fa2c1b3d7c90d2f9722fcae26040a3e778d05f
Alex Kozack <kozackunisoft@gmail.com>
2019-08-03 09:38:41 +0300

    Изменение иконки при переходе между страницами

--- src/UI/pages/history.vue
+++ src/UI/pages/history.vue
@@ -60,6 +60,7 @@ export default {
 
   beforeCreate() {
     document.title = "История просмотров";
+    document.head.querySelector('link[rel="icon"]').href = `/icons/play.png`;
   },
 
   async mounted() {
