772992916672433ee501fd2435a400f44d6f817d
Alex Kozack <kozackunisoft@gmail.com>
2019-08-14 18:56:03 +0300

    Fix Sentry deployment

--- deploy/sentry.sh
+++ deploy/sentry.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-VERSION=${TRAVIS_TAG:1}
+VERSION="Play Шикимори Online@${TRAVIS_TAG:1}"
+echo version is "$VERSION"
 
 # Create a release
-npx sentry-cli releases new -p extension $VERSION
-npx sentry-cli releases -p extension files $VERSION upload-sourcemaps ./dist/chrome/
-npx sentry-cli releases -p extension set-commits --commit $TRAVIS_REPO_SLUG@$TRAVIS_COMMIT $VERSION
-npx sentry-cli releases -p extension deploys $VERSION new -e production
+npx sentry-cli releases new -p extension "$VERSION"
+npx sentry-cli releases -p extension files "$VERSION" upload-sourcemaps ./dist/chrome/
+npx sentry-cli releases -p extension set-commits --commit $TRAVIS_REPO_SLUG@$TRAVIS_COMMIT "$VERSION"
+npx sentry-cli releases -p extension deploys "$VERSION" new -e production
