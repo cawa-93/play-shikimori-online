85c63fd64bdb8f0b4e71b2afeb6d270cb6bb56f8
Alex Kozack <kozackunisoft@gmail.com>
2019-08-28 23:42:49 +0300

    Форматирование

--- src/manifest.js
+++ src/manifest.js
@@ -1,104 +1,104 @@
 const permissions = [
-	'webRequest',
-	'webRequestBlocking',
-	'storage',
-	'tabs',
+    'webRequest',
+    'webRequestBlocking',
+    'storage',
+    'tabs',
 ]
 
 const origins = [
-	'https://shikimori.one/*',
-	'https://smotret-anime-365.ru/*',
-	'https://api.jikan.moe/*',
+    'https://shikimori.one/*',
+    'https://smotret-anime-365.ru/*',
+    'https://api.jikan.moe/*',
 ]
 
 const manifest = {
-	manifest_version: 2,
+    manifest_version: 2,
 
-	name: '__MSG_extName__',
+    name: '__MSG_extName__',
 
-	default_locale: 'ru',
+    default_locale: 'ru',
 
-	homepage_url: 'https://shikimori.one/clubs/2372',
+    homepage_url: 'https://shikimori.one/clubs/2372',
 
-	icons: {
-		'192': 'play.png',
-		'128': 'play-128.png',
-	},
+    icons: {
+        '192': 'play.png',
+        '128': 'play-128.png',
+    },
 
-	minimum_chrome_version: '73',
+    minimum_chrome_version: '73',
 
-	incognito: 'split',
+    incognito: 'split',
 
-	browser_action: {
-		default_title: 'Открыть историю просмотров',
-	},
+    browser_action: {
+        default_title: 'Открыть историю просмотров',
+    },
 
-	background: {
-		page: 'background.html',
-		persistent: true,
-	},
+    background: {
+        page: 'background.html',
+        persistent: true,
+    },
 
-	content_security_policy: `script-src 'self' 'unsafe-eval' https://ssl.google-analytics.com https://www.google-analytics.com; object-src 'self'`,
+    content_security_policy: `script-src 'self' 'unsafe-eval' https://ssl.google-analytics.com https://www.google-analytics.com; object-src 'self'`,
 
-	web_accessible_resources: [
-		'*',
-		'anime365-player-events.js',
-	],
+    web_accessible_resources: [
+        '*',
+        'anime365-player-events.js',
+    ],
 
-	permissions: [
-		...permissions,
-		...origins,
-	],
+    permissions: [
+        ...permissions,
+        ...origins,
+    ],
 
-	content_scripts: [
-		{
-			matches: [
-				'https://shikimori.org/*',
-				'https://shikimori.one/*',
-			],
-			js: [
-				'shikimori-watch-button.js',
-			],
-			run_at: 'document_idle',
-		},
-		{
-			matches: [
-				'https://smotret-anime-365.ru/translations/embed/*',
-				'https://hentai365.ru/translations/embed/*',
-			],
-			js: [
-				'anime-365-inject.js',
-			],
-			run_at: 'document_end',
-			all_frames: true,
-		},
-		// 	{
-		// 		matches: [
-		// 			'https://myanimelist.net/anime/*',
-		// 		],
-		// 		js: [
-		// 			'watch-button-myanime-list.js',
-		// 		],
-		// 		run_at: 'document_end',
-		// 	},
-	],
+    content_scripts: [
+        {
+            matches: [
+                'https://shikimori.org/*',
+                'https://shikimori.one/*',
+            ],
+            js: [
+                'shikimori-watch-button.js',
+            ],
+            run_at: 'document_idle',
+        },
+        {
+            matches: [
+                'https://smotret-anime-365.ru/translations/embed/*',
+                'https://hentai365.ru/translations/embed/*',
+            ],
+            js: [
+                'anime-365-inject.js',
+            ],
+            run_at: 'document_end',
+            all_frames: true,
+        },
+        // 	{
+        // 		matches: [
+        // 			'https://myanimelist.net/anime/*',
+        // 		],
+        // 		js: [
+        // 			'watch-button-myanime-list.js',
+        // 		],
+        // 		run_at: 'document_end',
+        // 	},
+    ],
 }
 
 if (process.env.BROWSER === 'firefox') {
 
-	manifest.browser_specific_settings = {
-		gecko: {
-			id: process.env.FIREFOX_EXTENSION_ID,
-			strict_min_version: '67.0',
-		},
-	}
+    manifest.browser_specific_settings = {
+        gecko: {
+            id: process.env.FIREFOX_EXTENSION_ID,
+            strict_min_version: '67.0',
+        },
+    }
 
-	manifest.incognito = 'spanning'
+    manifest.incognito = 'spanning'
 
 }
 
 module.exports = {
-	default: manifest,
-	permissions,
-	origins,
+    default: manifest,
+    permissions,
+    origins,
 }
