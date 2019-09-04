7e69942c1bac79eb62e7608ef4fcf2cc955ca255
Alex Kozack <kozackunisoft@gmail.com>
2019-09-04 13:29:28 +0300

    Fix Sentry Deploy

--- deploy/sentry.sh
+++ deploy/sentry.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#TRAVIS_TAG="v1.1.0"
+#TRAVIS_REPO_SLUG="cawa-93/play-shikimori-online"
+#TRAVIS_COMMIT="89a0454966c78e2c82c3f78e6259e73c48eb0dbc"
+
 VERSION="Play Шики Online@${TRAVIS_TAG:1}"
 echo version is "$VERSION"
 
