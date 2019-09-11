c29f58ab35478fb4fe7f7bb2ce5ffafa3dce4270
Alex Kozack <kozackunisoft@gmail.com>
2019-09-11 12:50:32 +0300

    Предварительная загрузка истории просмотров и избранных переводов в оперативную память

--- src/UI/store/index.ts
+++ src/UI/store/index.ts
@@ -3,17 +3,26 @@ import {sync} from '@/helpers/chrome-storage';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import Worker from 'worker-loader!./worker.ts';
+import {SelectedTranslation, WatchingHistoryItem} from '../../../types/UI';
 import {RootState} from './types';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store<RootState>({
     plugins: [
-        (store) => {
+        async (store) => {
             chrome.storage.onChanged.addListener((changes) => {
                 if (changes.userAuth) {
                     store.commit('profile/loadCredentialsFromServer', changes.userAuth.newValue);
                     store.dispatch('profile/loadUser');
+                }
+
+                if (changes.watching_history) {
+                    store.commit('player/setWatchingHistory', changes.watching_history.newValue || []);
+                }
+
+                if (changes.selectedTranslations) {
+                    store.commit('player/setSelectedTranslations', changes.selectedTranslations.newValue || []);
                 }
             });
 
@@ -26,6 +35,34 @@ export default new Vuex.Store<RootState>({
                     sync.set({userAuth: state.profile.credentials});
                 }
             });
+
+
+            const {
+                userAuth,
+                watching_history,
+                selectedTranslations,
+            } = await sync.get<{
+                userAuth: shikimori.Oauth | null
+                watching_history: WatchingHistoryItem[],
+                selectedTranslations: SelectedTranslation[],
+            }>({
+                userAuth: null,
+                watching_history: [],
+                selectedTranslations: [],
+            });
+
+            if (userAuth) {
+                store.commit('profile/loadCredentialsFromServer', userAuth);
+                store.dispatch('profile/loadUser');
+            }
+
+            if (watching_history) {
+                store.commit('player/setWatchingHistory', watching_history);
+            }
+
+            if (selectedTranslations) {
+                store.commit('player/setSelectedTranslations', selectedTranslations);
+            }
         },
     ],
 });
