1a0aeca0bdeaed0d0c66c8a8c5e22a8cb0cb75cb
Alex Kozack <kozackunisoft@gmail.com>
2019-09-18 13:41:11 +0300

    Доработана страница с настройками

---
+++ src/UI/components/options-controllers/theme.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-list
+        flat
+        subheader
+        two-line
+    >
+        <v-subheader>Внешний вид</v-subheader>
+
+        <v-list-item-group
+            multiple
+            v-model="theme"
+        >
+            <v-list-item value="dark">
+                <template v-slot:default="{ active, toggle }">
+                    <v-list-item-action>
+                        <v-checkbox
+                            color="primary"
+                            v-model="active"
+                        ></v-checkbox>
+                    </v-list-item-action>
+
+                    <v-list-item-content>
+                        <v-list-item-title>Включить темную тему</v-list-item-title>
+                        <v-list-item-subtitle>Все елементы управления на всех страницах будут иметь приглушенные тона</v-list-item-subtitle>
+                    </v-list-item-content>
+                </template>
+            </v-list-item>
+
+        </v-list-item-group>
+    </v-list>
+</template>
+
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+
+    @Component({
+        name: 'theme-option',
+    })
+    export default class ThemeOption extends Vue {
+        get theme() {
+            return this.$vuetify.theme.dark ? ['dark'] : [];
+        }
+
+        set theme(v) {
+            const isDark = v.includes('dark');
+            this.$vuetify.theme.dark = isDark;
+            localStorage.setItem('theme', isDark ? 'dark' : 'light');
+        }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/UI/views/Options.vue
+++ src/UI/views/Options.vue
@@ -1,122 +1,25 @@
 <template>
     <v-app id="app">
+        <v-container class="flex pb-3" id="page-content">
+            <login-option class="mb-3"></login-option>
+            <theme-option class="mb-3"></theme-option>
 
-        <v-list
-            flat
-            subheader
-            two-line
-        >
-
-            <v-subheader>Синхронизация прогресса</v-subheader>
-
-            <v-list-item-group
-                multiple
-                v-model="sync"
-            >
-                <v-list-item>
-                    <template v-slot:default="{ active, toggle }">
-                        <v-list-item-action>
-                            <v-checkbox
-                                @click="toggle"
-                                color="primary"
-                                v-model="active"
-                            ></v-checkbox>
-                        </v-list-item-action>
-
-                        <v-list-item-content>
-                            <v-list-item-title>Синхронизировать с Шикимори</v-list-item-title>
-                            <v-list-item-subtitle>Все просмотренные серии будут автоматически добавлены в ваш список на shikimori.one</v-list-item-subtitle>
-                        </v-list-item-content>
-
-                        <v-list-item-action>
-                            <v-avatar v-if="user && active">
-                                <v-img :src="user.avatar"></v-img>
-                            </v-avatar>
-
-                        </v-list-item-action>
-                    </template>
-                </v-list-item>
-
-            </v-list-item-group>
-        </v-list>
-
-        <v-list
-            class="mt-3"
-            flat
-            subheader
-            two-line
-        >
-
-            <v-subheader>Внешний вид</v-subheader>
-
-            <v-list-item-group
-                multiple
-                v-model="theme"
-            >
-                <v-list-item>
-                    <template v-slot:default="{ active, toggle }">
-                        <v-list-item-action>
-                            <v-checkbox
-                                @click="toggle"
-                                color="primary"
-                                v-model="active"
-                            ></v-checkbox>
-                        </v-list-item-action>
-
-                        <v-list-item-content>
-                            <v-list-item-title>Включить темную тему</v-list-item-title>
-                        </v-list-item-content>
-                    </template>
-                </v-list-item>
-
-            </v-list-item-group>
-        </v-list>
-
+            <footer class="d-flex pt-3 justify-center">
+                <v-btn href="https://github.com/cawa-93/play-shikimori-online/wiki/FAQ" outlined rounded>FAQ</v-btn>
+            </footer>
+        </v-container>
     </v-app>
 </template>
 
 <script lang="ts">
-    import profileStore from '@/UI/store/profile';
+    import LoginOption from '@/UI/components/options-controllers/login.vue';
+    import ThemeOption from '@/UI/components/options-controllers/theme.vue';
     import {Component, Vue} from 'vue-property-decorator';
 
-
-    @Component
+    @Component({
+        components: {LoginOption, ThemeOption},
+    })
     export default class Options extends Vue {
-        public syncPrivate: number[] = [];
-
-        get user() {
-            return profileStore.user;
-        }
-
-        get sync() {
-            return this.user ? [0] : [];
-        }
-
-        set sync(v) {
-            v.length ? this.logIn() : this.logout();
-            this.syncPrivate = v;
-        }
-
-        get theme() {
-            return this.$vuetify.theme.dark ? [0] : [];
-        }
-
-        set theme(v) {
-            // @ts-ignore
-            this.$vuetify.theme.dark = v.length > 0;
-            localStorage.setItem('theme', v.length > 0 ? 'dark' : 'light');
-        }
-
-
-        public logIn() {
-            return profileStore.getValidCredentials(true);
-        }
-
-        public logout() {
-            return profileStore.logout();
-        }
-
-
     }
 </script>
 
