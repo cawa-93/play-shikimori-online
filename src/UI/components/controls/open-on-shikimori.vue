9fad7d5f1583926df221cb6ef397ebae1b483c15
Alex Kozack <kozackunisoft@gmail.com>
2019-10-30 11:38:17 +0200

    Удален скелетон для кнопки "Открыть Шикимори"

--- src/UI/components/controls/open-on-shikimori.vue
+++ src/UI/components/controls/open-on-shikimori.vue
@@ -1,31 +1,25 @@
 <template>
-    <div class="flex-parent open-on-shikimori">
-        <v-btn
-            :disabled="!shikiID"
-            :href="`https://shikimori.one/animes/${shikiID}`"
-            @click.stop
-            aria-label="Открыть на Шикимори"
-
-            text
-            v-if="readyToShow"
-        >
-            <img height="24px" id="shikimori-logo-light" src="/shikimori-logo.svg" width="24px">
-            <span class="long-and-truncated ml-2" v-if="!compact">Открыть на Шикимори</span>
-        </v-btn>
-        <v-skeleton-loader min-width="100%" type="button" v-else></v-skeleton-loader>
-    </div>
+    <v-btn
+        :disabled="!shikiID"
+        :href="`https://shikimori.one/animes/${shikiID}`"
+        @click.stop
+        aria-label="Открыть на Шикимори"
+        class="open-on-shikimori"
+        text
+    >
+        <img height="24px" id="shikimori-logo-light" src="/shikimori-logo.svg" width="24px">
+        <span class="long-and-truncated ml-2" v-if="!compact">Открыть на Шикимори</span>
+    </v-btn>
 </template>
 
 <script lang="ts">
-    import Boilerplate from '@/UI/mixins/boilerplate';
     import playerStore from '@/UI/store/player';
-    import {mixins} from 'vue-class-component';
-    import {Component, Prop, Watch} from 'vue-property-decorator';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
 
     @Component({
         name: 'open-on-shikimori',
     })
-    export default class OpenOnShikimori extends mixins(Boilerplate) {
+    export default class OpenOnShikimori extends Vue {
         @Prop() public readonly compact!: boolean;
 
         get shikiID() {
@@ -35,16 +29,11 @@
 
             return this.$route.params.anime;
         }
-
-        @Watch('shikiID')
-        public nextEpisodeOnChange() {
-            this.readyToShow = true;
-        }
     }
 </script>
 
 <style scoped>
-    .open-on-shikimori .theme--dark #shikimori-logo-light {
+    .open-on-shikimori.theme--dark #shikimori-logo-light {
         filter: invert(1);
     }
 </style>
