18b60d3763526ec2d57ed8f677b6cf235279d21b
Alex Kozack <kozackunisoft@gmail.com>
2019-10-30 11:33:08 +0200

    Удален скелетон для кнопки Скачать

--- src/UI/components/controls/download.vue
+++ src/UI/components/controls/download.vue
@@ -1,46 +1,43 @@
 <template>
-    <div class="flex-parent download-video">
-        <v-menu v-if="readyToShow">
-            <template v-slot:activator="{ on }">
-                <v-btn
-                    :disabled="!download.length"
-                    aria-label="Скачать"
-                    text
-                    v-on="on"
-                >
-                    <v-icon>mdi-download</v-icon>
-                    <span class="long-and-truncated  ml-2" v-if="!compact">Скачать</span>
-                </v-btn>
-            </template>
+    <v-menu>
+        <template v-slot:activator="{ on, attr }">
+            <v-btn
+                :disabled="!download.length"
+                aria-label="Скачать"
+                class="download-video"
+                text
+                v-bind="attr"
+                v-on="on"
+            >
+                <v-icon>mdi-download</v-icon>
+                <span class="long-and-truncated  ml-2" v-if="!compact">Скачать</span>
+            </v-btn>
+        </template>
 
-            <v-list>
-                <v-list-item
-                    :href="item.url"
-                    :key="index"
-                    download
-                    target="_self"
-                    v-for="(item, index) in download"
-                >
-                    <v-list-item-title>{{ item.height }}p</v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-menu>
-        <v-skeleton-loader type="button" v-else></v-skeleton-loader>
-    </div>
+        <v-list>
+            <v-list-item
+                :href="item.url"
+                :key="index"
+                download
+                target="_self"
+                v-for="(item, index) in download"
+            >
+                <v-list-item-title>{{ item.height }}p</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-menu>
 </template>
 
 <script lang="ts">
     import {Anime365Provider} from '@/helpers/API/Anime365Provider';
-    import Boilerplate from '@/UI/mixins/boilerplate';
     import playerStore from '@/UI/store/player';
-    import {mixins} from 'vue-class-component';
-    import {Component, Prop, Watch} from 'vue-property-decorator';
+    import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
 
 
     @Component({
         name: 'download-video',
     })
-    export default class DownloadVideo extends mixins(Boilerplate) {
+    export default class DownloadVideo extends Vue {
         @Prop() public readonly compact!: boolean;
 
         public download: anime365.DownloadItem[] = [];
@@ -69,8 +66,6 @@
             } else {
                 this.download = [];
             }
-
-            this.readyToShow = true;
         }
     }
 </script>
