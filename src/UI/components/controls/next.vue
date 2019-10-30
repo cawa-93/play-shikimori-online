64b6961ed02dd5c8b9cb8766a25944655d9da274
Alex Kozack <kozackunisoft@gmail.com>
2019-10-30 11:36:02 +0200

    Удален скелетон для кнопки "Следующая серия"

--- src/UI/components/controls/next.vue
+++ src/UI/components/controls/next.vue
@@ -1,82 +1,70 @@
 <template>
-    <div class="next-episode">
-        <v-btn
-            :aria-label="label"
-            :disabled="disabled"
-            @click.prevent="gotoNext"
-            target="_self"
-            text
-            v-if="readyToShow"
-        >
-            <span class="long-and-truncated mr-2" v-if="!compact">{{label}}</span>
-            <v-icon>mdi-skip-next</v-icon>
-        </v-btn>
-        <v-skeleton-loader type="button" v-else></v-skeleton-loader>
-    </div>
+    <v-btn
+        :aria-label="label"
+        :disabled="disabled"
+        @click.prevent="gotoNext"
+        class="next-episode"
+        target="_self"
+        text
+    >
+        <span class="long-and-truncated mr-2" v-if="!compact">{{label}}</span>
+        <v-icon>mdi-skip-next</v-icon>
+    </v-btn>
 </template>
 
 <script lang="ts">
-import Boilerplate from '@/UI/mixins/boilerplate';
-import playerStore from '@/UI/store/player';
-import shikimoriStore from '@/UI/store/shikimori';
-import {mixins} from 'vue-class-component';
-import {Component, Prop, Watch} from 'vue-property-decorator';
+    import playerStore from '@/UI/store/player';
+    import shikimoriStore from '@/UI/store/shikimori';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
 
-@Component
-export default class Next extends mixins(Boilerplate) {
-    @Prop() public readonly compact!: boolean;
+    @Component
+    export default class Next extends Vue {
+        @Prop() public readonly compact!: boolean;
 
 
-    get nextEpisode() {
-        return playerStore.currentEpisode ? playerStore.currentEpisode.next : null;
-    }
-
-    get nextSeason() {
-        return shikimoriStore.nextSeason;
-    }
-
-    get disabled() {
-        return !this.nextEpisode && !this.nextSeason;
-    }
-
-    get label() {
-        if (this.nextSeason && !this.nextEpisode) {
-            return 'Следующий сезон';
+        get nextEpisode() {
+            return playerStore.currentEpisode ? playerStore.currentEpisode.next : null;
         }
 
-        return 'Следующая серия';
-    }
-
-    public gotoNext() {
-        shikimoriStore.markAsWatched();
-
-        if (this.nextEpisode) {
-            return playerStore.selectNextEpisode();
+        get nextSeason() {
+            return shikimoriStore.nextSeason;
         }
 
-        if (this.nextSeason) {
-            playerStore.clear();
-
-            return Promise.all([
-                // Загрузка списка серий и запуск видео
-                playerStore.loadEpisodes({
-                    anime: this.nextSeason.id,
-                    episode: this.nextSeason.episodeInt || 0,
-                }),
-
-                // Загрузка информации про аниме и оценку от пользователя если тот авторизован
-                shikimoriStore.loadAnime(this.nextSeason.id),
-            ]);
-
-
+        get disabled() {
+            return !this.nextEpisode && !this.nextSeason;
         }
 
+        get label() {
+            if (this.nextSeason && !this.nextEpisode) {
+                return 'Следующий сезон';
+            }
 
-    }
+            return 'Следующая серия';
+        }
 
-    @Watch('nextEpisode')
-    public nextEpisodeOnChange() {
-        this.readyToShow = true;
+        public gotoNext() {
+            shikimoriStore.markAsWatched();
+
+            if (this.nextEpisode) {
+                return playerStore.selectNextEpisode();
+            }
+
+            if (this.nextSeason) {
+                playerStore.clear();
+
+                return Promise.all([
+                    // Загрузка списка серий и запуск видео
+                    playerStore.loadEpisodes({
+                        anime: this.nextSeason.id,
+                        episode: this.nextSeason.episodeInt || 0,
+                    }),
+
+                    // Загрузка информации про аниме и оценку от пользователя если тот авторизован
+                    shikimoriStore.loadAnime(this.nextSeason.id),
+                ]);
+
+
+            }
+        }
     }
-}
 </script>
