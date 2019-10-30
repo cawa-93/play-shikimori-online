4c8110d1bc5bc092221ce79d4ffa6e2bf2da1dca
Alex Kozack <kozackunisoft@gmail.com>
2019-10-30 11:39:40 +0200

    Удален скелетон для кнопки "Настройки"

--- src/UI/components/controls/preference.vue
+++ src/UI/components/controls/preference.vue
@@ -1,29 +1,23 @@
 <template>
-    <div class="flex-parent preference">
-        <v-btn
-            @click="openOptionsPage"
-            aria-label="Настройки"
-            text
-            v-if="readyToShow"
-        >
-            <v-icon :color="!user ? 'error' : ''">mdi-{{user ? 'settings' : 'sync-off'}}</v-icon>
-            <span class="long-and-truncated ml-2" v-if="!compact">Настройки</span>
-        </v-btn>
-        <v-skeleton-loader type="button" v-else></v-skeleton-loader>
-    </div>
-
+    <v-btn
+        @click="openOptionsPage"
+        aria-label="Настройки"
+        class="preference"
+        text
+    >
+        <v-icon :color="!user ? 'error' : ''">mdi-{{user ? 'settings' : 'sync-off'}}</v-icon>
+        <span class="long-and-truncated ml-2" v-if="!compact">Настройки</span>
+    </v-btn>
 </template>
 
 <script lang="ts">
-    import Boilerplate from '@/UI/mixins/boilerplate';
     import profileStore from '@/UI/store/profile';
-    import {mixins} from 'vue-class-component';
-    import {Component, Prop} from 'vue-property-decorator';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
 
     @Component({
         name: 'preference',
     })
-    export default class Preference extends mixins(Boilerplate) {
+    export default class Preference extends Vue {
         @Prop() public readonly compact!: boolean;
 
         get user() {
@@ -32,10 +26,6 @@
 
         public openOptionsPage() {
             return chrome.runtime.openOptionsPage();
-        }
-
-        public mounted() {
-            this.readyToShow = true;
         }
     }
 </script>
