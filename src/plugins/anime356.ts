aac9baa8c7c9c2ac6116458b4d36344953a4ec91
Alex Kozack <kozackunisoft@gmail.com>
2019-11-13 12:55:34 +0200

    Axios модуль для anime365

---
+++ src/plugins/anime356.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+
+export const shikimori = axios.create({
+    baseURL: 'https://smotret-anime.online/api',
+    // headers: {'User-Agent': 'play-shiki-app'},
+});
+
+axiosRetry(shikimori, {
+    retries: 5,
+    retryDelay: (c: number) => c * 1000,
+});
+
