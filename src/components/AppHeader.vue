5180450536a7a91db1ccf3dd76e35221626c9310
Alex Kozack <kozackunisoft@gmail.com>
2019-11-25 16:48:27 +0200

    Базовая шапка

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar app flat>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-bell</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-settings</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class AppHeader extends Vue {
+
+
+
+  }
+</script>
